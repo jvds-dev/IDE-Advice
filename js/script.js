35850881c5a60b1cb0b893f65ac3383be13d2fbb
João Victor <71608479+JVDS2@users.noreply.github.com>
2022-12-19 14:54:50 -0300

    slider

--- js/script.js
+++ js/script.js
@@ -1,7 +1,33 @@
-let menu = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
+const controls = document.querySelectorAll(".control");
+let currentItem = 0;
+const items = document.querySelectorAll(".item");
+const maxItems = items.length;
 
-menu.onclick = () => {
-    menu.classList.toggle('bx-x')
-    navbar.classList.toggle('open')
-}
+controls.forEach((control) => {
+  control.addEventListener("click", (e) => {
+    isLeft = e.target.classList.contains("arrow-left");
+
+    if (isLeft) {
+      currentItem -= 1;
+    } else {
+      currentItem += 1;
+    }
+
+    if (currentItem >= maxItems) {
+      currentItem = 0;
+    }
+
+    if (currentItem < 0) {
+      currentItem = maxItems - 1;
+    }
+
+    items.forEach((item) => item.classList.remove("current-item"));
+
+    items[currentItem].scrollIntoView({
+      behavior: "smooth",
+      inline: "start"
+    });
+
+    items[currentItem].classList.add("current-item");
+  });
+});
