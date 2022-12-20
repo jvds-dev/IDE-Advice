06a1eeafd880eac59108cf1fee70b410e44841a1
João Victor <71608479+JVDS2@users.noreply.github.com>
2022-12-20 13:54:58 -0300

    figma ver

--- js/script.js
+++ js/script.js
@@ -5,10 +5,23 @@ $('.gallery').slick({
   slidesToScroll: 1,
   responsive: [
     {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 3,
+      }
+    },
+    
+    {
       breakpoint: 640,
       settings: {
         slidesToShow: 2,
-        slidesToScroll: 1,
       },
-    },]
+    },
+    {
+      breakpoint: 400,
+      settings: {
+        slidesToShow: 1,
+      }
+    }
+  ]
 });
