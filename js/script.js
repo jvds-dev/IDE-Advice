5df5d15b6e9d670af9ff5ab36ad25cbe61a8e7e9
João Victor <71608479+JVDS2@users.noreply.github.com>
2022-12-20 09:03:10 -0300

    responsive slickslider

--- js/script.js
+++ js/script.js
@@ -2,5 +2,13 @@ $(".galeria").slick();
 $('.gallery').slick({
   infinite: true,
   slidesToShow: 4,
-  slidesToScroll: 1
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 640,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      },
+    },]
 });
